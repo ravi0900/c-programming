734161c0e601175ceb7a4cc0da6dc6922eea84f8
Ravi <65914575+ravi0900@users.noreply.github.com>
2022-07-30 00:20:13 +0530

    cprogramming

---
+++ Chapter 3 - Conditional Instructions/output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+
+int main(){
+    int var1 = 5;
+    int var2 = 6;
+    bool x= var1>var2;
+    fputs(x ? "true" : "false", stdout);
+    
+
+}
+//intresting program...
