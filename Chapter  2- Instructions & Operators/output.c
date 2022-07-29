16e486f771aaaf517562b76cfe8fef5d9039aefe
Ravi <65914575+ravi0900@users.noreply.github.com>
2022-07-30 00:21:41 +0530

    cprogramming

---
+++ Chapter  2- Instructions & Operators/output.c
@@ -0,0 +1,36 @@
+// #include<stdio.h>
+
+// int main(){
+//     double a = 6 / 4;
+//     int b  = 6 / 4;
+//     double c = a + b;
+//     printf("%lf",c);
+// return 0;
+// }
+
+//you might think is 2.5, but right answer is 2 sicnce operator int/int always give int value. hence even after double for variable a the output by 6/4 is 1.000000
+
+// #include<stdio.h>
+
+// int main(){
+//     double a = 55.5;
+//     int b = 55;
+//     a = a % 10;
+//     b = b % 10;
+//     printf("%lf \n %lf", a,b);
+// return 0;
+// }
+
+// this will lead to syntax error since % operator cant be used with float or double
+
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(){
+bool x = true;
+
+printf("%d\n", x);
+return 0;
+}
+
+
