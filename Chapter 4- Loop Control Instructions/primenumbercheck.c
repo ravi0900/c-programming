58c83ed492f209168dccc2e4e12690b4e5bd4e50
Ravi <65914575+ravi0900@users.noreply.github.com>
2022-08-02 23:57:35 +0530

    cprogramming

--- Chapter 4- Loop Control Instructions/primenumbercheck.c
+++ Chapter 4- Loop Control Instructions/primenumbercheck.c
@@ -4,15 +4,17 @@ int main(){
     int i=1,prime=1,n;
     scanf("%d",&n);
     for(i;i<=n;i++){
-        if(i%2==0){
+        if(n%2==0){
             printf("this is not a prime number%d",n);
-            break;
         }
         else{
             printf("this is prime number %d",n);
         
 
         }
+        break;
     }
+    // int f=134.287473278;
+    // printf("%e",134.287473278);
 return 0;
 }   
