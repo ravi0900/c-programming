96716ed2bd1add17a54fb5cb132fc8f8a85f660d
Ravi <65914575+ravi0900@users.noreply.github.com>
2022-08-09 12:30:38 +0530

    cprogramming

---
+++ Chapter 3 - Conditional Instructions/coditions.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+   
+    int a = 10, b = 20, c = 30;
+    if(a <= b && !b) {
+        printf("hello");
+    }
+    else if(c >= a && c >= b) {
+
+        printf("hi");
+
+    }
+    else {
+        
+        printf("hey");
+
+    }
+
+return 0;
+}
+
+//try to find out yourself
