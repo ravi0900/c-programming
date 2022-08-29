ece4625ad2403c7fb7906d30b13f6c088b49b7ce
Ravi <65914575+ravi0900@users.noreply.github.com>
2022-08-29 16:58:29 +0530

    cprogramming

---
+++ Chapter 4- Loop Control Instructions/8whilerepeat.c
@@ -0,0 +1,18 @@
+// #include<stdio.h>
+
+// int main(){
+//     int k=8;
+//     for (k;;)
+//     printf("%d",k);
+// return 0;
+// }
+
+#include<stdio.h>
+
+int main(){
+    int k=8;
+    while(k)
+    printf("%d",k);
+
+return 0;
+}
